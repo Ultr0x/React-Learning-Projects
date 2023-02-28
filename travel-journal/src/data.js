13e4a5045055ff296f8ca2b1ea56eba01cf23de3
Jan <janeksus21@gmail.com>
2023-02-28 21:50:22 +0100

    Created Meme Generator

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -25,5 +25,14 @@ export default [
         description : "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         date : "2021-06-01",
         link : "https://www.google.com/search?q=kashmir+tourism&rlz=1C1CHBF_enIN921IN921&oq=kashmir+&aqs=chrome.0.69i59j69i57j0l4.1001j0j7&sourceid=chrome&ie=UTF-8"
+    },
+    {
+        id: 4,
+        country : "Poland",
+        place : "Kashmir",
+        image : "../images/fuji.png",
+        description : "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        date : "2021-06-01",
+        link : "https://www.google.com/search?q=kashmir+tourism&rlz=1C1CHBF_enIN921IN921&oq=kashmir+&aqs=chrome.0.69i59j69i57j0l4.1001j0j7&sourceid=chrome&ie=UTF-8"
     }
 ]
